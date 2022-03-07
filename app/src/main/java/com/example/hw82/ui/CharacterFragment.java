ac2f046f8a6f74b191bc192a366c9192419a6b83
Sanzhar <sanchous048@gmail.com>
2022-03-07 22:44:37 +0600

    фывфы

--- app/src/main/java/com/example/hw82/ui/CharacterFragment.java
+++ app/src/main/java/com/example/hw82/ui/CharacterFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,6 +38,9 @@ public class CharacterFragment extends Fragment {
         LoadData();
         CharacterAdapter adapter= new CharacterAdapter(characterPeople);
         binding.recyclerCharacter.setAdapter(adapter);
+
+        @NonNull NavDirections action=CharacterFragmentDirections.actionCharacterToDetailsFragment();
+        NavHostFragment.findNavController(CharacterFragment.this).navigate(action);
     }
 
     private void LoadData() {
